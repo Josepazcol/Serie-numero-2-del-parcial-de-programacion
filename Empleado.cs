1266d83d4dbcf15d351098db1cafa459a024468c
DELL <DELL@DESKTOP-DV0BDKO>
2024-05-04 12:20:59 -0600

    Agregar archivos de proyecto.

---
+++ Empleado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Nómina_Empleados
+{
+    internal class Empleado
+    {
+        internal double CalcularSalario()
+        {
+            throw new NotImplementedException();
+        }
+
+        internal char ToStringGuardado()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
